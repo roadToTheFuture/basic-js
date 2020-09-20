d5cccf1ea10ef82efbfb0ef521aa9a767d095c61
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2020-09-20 22:05:50 +0300

    do carbon-dating: create dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const k = 0.693 / HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	if (typeof sampleActivity === 'string' && parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) < 15) {
+		let x = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) * 1000000) / 1000000;
+		let y = Math.ceil(x / k);
+		return y;
+	} else {
+		return false;
+	}
 };
