3d3ac20c0b8349a3c5064460b8d2a50679b3d9ad
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2020-10-02 19:47:58 +0300

    feat: add carbon-dating count-cats

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	function checkArr(arr) {
+		if (typeof arr[0] === 'string') {
+			return true;
+		} else {
+			return false;
+		}
+
+	};
+
+	module.exports = function createDreamTeam(members) {
+		if (checkArr(members)) {
+			let x = members;
+			let result = "";
+
+
+			for (let i = 0; i < x.length; i++) {
+				if (typeof x[i] === 'string') {
+					result += (/[A-Za-z]/).exec(x[i].split('')).toString().trim().toUpperCase();
+				}
+			}
+			return result.split('').sort().join('');
+
+		} else {
+			return false;
+		}
+	};
 };
